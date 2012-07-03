6b8b2ff6dcea5e399af6ae5ab8504b78a7b00314
Lachlan Musicman <datakid@gmail.com>
2012-07-03 15:32:21 +1200

    made the changes to search so that orgs and vacancies are included

--- ner/search_indexes.py
+++ ner/search_indexes.py
@@ -9,12 +9,29 @@ from ner.models import Person, Requirement, FTCQualification, Vacancy, Certifica
 
 
 class PersonIndex(SearchIndex):
-	text = CharField(document=True, use_template=True)
-	surname = CharField(model_attr='surname')
-	first_name = CharField(model_attr='first_name')
-	
-	def get_model(self):
-		return Person
+    text = CharField(document=True, use_template=True)
+    surname = CharField(model_attr='surname')
+    first_name = CharField(model_attr='first_name')
 
+    def get_model(self):
+        return Person
+
+class OrganisationIndex(SearchIndex):
+    text = CharField(document=True,use_template=True)
+    name = CharField(model_attr='name')
+
+    def get_model(self):
+        return Organisation
+
+class VacancyIndex(SearchIndex):
+    text = CharField(document=True,use_template=True)
+    title = CharField(model_attr='title')
+    closing_date = DateField(model_attr='closing_date')
+    organisation = CharField(model_attr='organisation')
+
+    def get_model(self):
+        return Vacancy
 
 site.register(Person, PersonIndex)
+site.register(Vacancy, VacancyIndex)
+site.register(Organisation, OrganisationIndex)
